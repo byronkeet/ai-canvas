26f87573d5789aa418f885bceac003072e29312c
Byron Keet <keetbis@gmail.com>
2023-01-20 16:54:42 +0200

    Installed Flowbite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+	"./src/**/*.{js,ts,jsx,tsx}",
+	"./node_modules/flowbite-react/**/*.js",
+],
   theme: {
     extend: {},
+	fontFamily: {
+		display: ["Jura", "Roboto"],
+	},
   },
-  plugins: [],
+  plugins: [
+	require("flowbite/plugin")
+  ],
 };
