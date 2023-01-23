4240e8b353e3b6f81dc0b0276572016ce826e09f
Byron Keet <keetbis@gmail.com>
2023-01-23 10:39:41 +0200

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
 			<div className="sm:flex sm:items-center sm:justify-between">
 				<Link href="/" className="flex items-center mb-4 sm:mb-0">
 					<Image src="/logo.png" className="w-auto h-8 mr-3" alt="Artificality Logo" width={40} height={40}/>
-					<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Artificality</span>
+					<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white font-display uppercase">Artificality</span>
 				</Link>
 				<ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
 					{menuItems.map(item => (
@@ -22,7 +22,7 @@ const Footer = () => {
 				</ul>
 			</div>
 			<hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-			<span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 3 <a href="https://Artificality.com/" className="hover:underline">Artificality™</a>. All Rights Reserved.
+			<span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© <Link href="/" className="hover:underline">Artificality™</Link>. All Rights Reserved.
 			</span>
 			</div>
 		</footer>
